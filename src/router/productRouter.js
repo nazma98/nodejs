0c7206a5744fcb886f389cb6bb88ee78ae9ffd2c
kmtusher97 <kmtusher97@gmail.com>
2025-02-16 21:53:19 +0600

    Add service layer

--- src/router/productRouter.js
+++ src/router/productRouter.js
@@ -1,66 +1,26 @@
 const express = require('express');
-const { v4: uuidv4 } = require('uuid');
+
+const { productServices } = require('../service');
 
 const productRouter = express.Router();
 
-const products = [
-  {
-    _id: '8f9a5e60-2b6d-4051-9ff4-1aca15bd8e01',
-    name: 'baked beans',
-    price: 0.4,
-    image: 'beans.jpg',
-    type: 'vegetables',
-  },
-  {
-    _id: 'fda32bb2-c6be-4e6a-abeb-2dca8fca3e83',
-    name: 'hot dogs',
-    price: 1.99,
-    image: 'hotdogs.jpg',
-    type: 'meat',
-  },
-];
-
 productRouter.get('/', (req, res) => {
-  res.send(products);
+  res.send(productServices.getAllProducts());
 });
 
 productRouter.post('/', (req, res) => {
-  const newProductData = req.body;
-  const newProduct = { _id: uuidv4(), ...newProductData };
-  products.unshift(newProduct);
-
-  res.status(201).json(newProduct);
+  res.status(201).json(productServices.createProduct(req.body));
 });
 
 productRouter.put('/:id', (req, res) => {
   const { id } = req.params;
-  const payload = req.body;
-
-  let updatedProductIndex = products.findIndex((product) => product._id === id);
-
-  if (updatedProductIndex === -1) {
-    return res.status(400).json({ message: `No product exists with id ${id}` });
-  }
-
-  products[updatedProductIndex] = {
-    ...products[updatedProductIndex],
-    ...payload,
-  };
-
-  res.status(201).json(products[updatedProductIndex]);
+  const updatedProduct = productServices.updateProduct(id, req.body);
+  res.status(201).json(updatedProduct);
 });
 
 productRouter.delete('/:id', (req, res) => {
   const { id } = req.params;
-
-  let productIndex = products.findIndex((product) => product._id === id);
-
-  if (productIndex === -1) {
-    return res.status(400).json({ message: `No product exists with id ${id}` });
-  }
-
-  products.splice(productIndex, 1);
-
+  productServices.deleteProduct(id);
   res.status(201).json({ message: 'Product has been deleted' });
 });
 
---
+++ src/service/productServices.js
@@ -0,0 +1,60 @@
+const { v4: uuidv4 } = require('uuid');
+
+const products = [
+  {
+    _id: '8f9a5e60-2b6d-4051-9ff4-1aca15bd8e01',
+    name: 'baked beans',
+    price: 0.4,
+    image: 'beans.jpg',
+    type: 'vegetables',
+  },
+  {
+    _id: 'fda32bb2-c6be-4e6a-abeb-2dca8fca3e83',
+    name: 'hot dogs',
+    price: 1.99,
+    image: 'hotdogs.jpg',
+    type: 'meat',
+  },
+];
+
+const createProduct = (productPayload) => {
+  const newProduct = { _id: uuidv4(), ...productPayload };
+  products.unshift(newProduct);
+  return newProduct;
+};
+
+const getAllProducts = () => products;
+
+const updateProduct = (id, payload) => {
+  let updatedProductIndex = products.findIndex((product) => product._id === id);
+
+  if (updatedProductIndex === -1) {
+    throw new Error(`No product exists with id ${id}`);
+  }
+
+  products[updatedProductIndex] = {
+    ...products[updatedProductIndex],
+    ...payload,
+  };
+
+  return products[updatedProductIndex];
+};
+
+const deleteProduct = (id) => {
+  let productIndex = products.findIndex((product) => product._id === id);
+
+  if (productIndex === -1) {
+    return res.status(400).json({ message: `No product exists with id ${id}` });
+  }
+
+  products.splice(productIndex, 1);
+
+  return true;
+};
+
+module.exports = {
+  createProduct,
+  deleteProduct,
+  getAllProducts,
+  updateProduct,
+};
