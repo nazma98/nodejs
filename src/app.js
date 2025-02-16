84b500ca9ac6b4f71fec7d5181f26146e0bd710b
kmtusher97 <kmtusher97@gmail.com>
2025-02-16 21:30:47 +0600

    Add router

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const { v4: uuidv4 } = require('uuid');
+
+const { productRouter, userRouter } = require('./router');
 
 const port = 8000;
 
@@ -7,70 +8,12 @@ const app = express();
 
 app.use(express.json());
 
-const products = [
-  {
-    _id: '8f9a5e60-2b6d-4051-9ff4-1aca15bd8e01',
-    name: 'baked beans',
-    price: 0.4,
-    image: 'beans.jpg',
-    type: 'vegetables',
-  },
-  {
-    _id: 'fda32bb2-c6be-4e6a-abeb-2dca8fca3e83',
-    name: 'hot dogs',
-    price: 1.99,
-    image: 'hotdogs.jpg',
-    type: 'meat',
-  },
-];
-
 app.get('/status', (req, res) => {
   res.send('OK');
 });
 
-app.get('/api/products', (req, res) => {
-  res.send(products);
-});
-
-app.post('/api/products', (req, res) => {
-  const newProductData = req.body;
-  const newProduct = { _id: uuidv4(), ...newProductData };
-  products.unshift(newProduct);
-
-  res.status(201).json(newProduct);
-});
-
-app.put('/api/products/:id', (req, res) => {
-  const { id } = req.params;
-  const payload = req.body;
-
-  let updatedProductIndex = products.findIndex((product) => product._id === id);
-
-  if (updatedProductIndex === -1) {
-    return res.status(400).json({ message: `No product exists with id ${id}` });
-  }
-
-  products[updatedProductIndex] = {
-    ...products[updatedProductIndex],
-    ...payload,
-  };
-
-  res.status(201).json(products[updatedProductIndex]);
-});
-
-app.delete('/api/products/:id', (req, res) => {
-  const { id } = req.params;
-
-  let productIndex = products.findIndex((product) => product._id === id);
-
-  if (productIndex === -1) {
-    return res.status(400).json({ message: `No product exists with id ${id}` });
-  }
-
-  products.splice(productIndex, 1);
-
-  res.status(201).json({ message: 'Product has been deleted' });
-});
+app.use('/api/products', productRouter);
+app.use('/api/users', userRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
