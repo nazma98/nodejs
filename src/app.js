8fa2a54bfa5dd49301a92d2fb5af1756426f4e5c
kmtusher97 <kmtusher97@gmail.com>
2025-02-21 21:54:22 +0600

    Connect mongodb

--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
+require('dotenv').config();
+
 const express = require('express');
+const mongoose = require('mongoose');
 
 const { logRequestMiddleware, errorHandler } = require('./middleware');
 const { configureRouter } = require('./router');
@@ -6,6 +9,23 @@ const { configureRouter } = require('./router');
 const port = 8000;
 
 const app = express();
+
+const MONGO_URI = process.env.MONGO_URI;
+const MONGO_DB_NAME = process.env.MONGO_DB;
+
+const connectDB = () => {
+  console.log('Connecting mongodb...');
+  mongoose
+    .connect(MONGO_URI, {
+      dbName: MONGO_DB_NAME,
+    })
+    .then(() => {
+      console.log('MongoDB connected successfully');
+    })
+    .catch((err) => console.err(`MongoDB connection failed!! ${err}`));
+};
+
+connectDB();
 
 app.use(express.json());
 
