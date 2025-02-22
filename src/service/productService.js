00c2006e05e4f157e1e634477eb07c22890d8fb5
nazma98 <nazmasarker1311@gmail.com>
2025-02-22 23:59:51 +0600

    Add product service

---
+++ src/service/productService.js
@@ -0,0 +1,34 @@
+const { v4: uuidv4 } = require('uuid');
+
+const products = [
+    {
+        "_id": "7c08c0d2-5ac7-4cab-afbf-5b379a7f9e73",
+        "name": "beef",
+        "price": 1.4,
+        "image": "beef.jpg",
+        "type": "meat"
+    },
+    {
+        "_id": "eb64c741-6859-4ff8-b3c0-ff6f55b66b13",
+        "name": "baked beans",
+        "price": 0.4,
+        "image": "beans.jpg",
+        "type": "vegetables"
+    },
+    {
+        "_id": "b137eb7c-40ab-497f-836e-a69d4c709111",
+        "name": "hot dogs",
+        "price": 1.99,
+        "image": "hotdogs.jpg",
+        "type": "meat"
+    }
+];
+
+const getAllProducts = () => products;
+
+const createProduct = ( productPayload ) => {
+        const newProduct = { _id: uuidv4(), ...productPayload }
+        products.unshift(newProduct)
+
+        return newProduct;
+}
