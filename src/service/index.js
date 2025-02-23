22af2116fb59016ab15b962392557a484aa4d370
nazma98 <nazmasarker1311@gmail.com>
2025-02-23 11:57:30 +0600

    Add api endpoints to userRouter js

--- src/service/index.js
+++ src/service/index.js
@@ -1,5 +1,7 @@
 const productServices = require('./productServices');
+const userServices = require('./userServices');
 
 module.exports = {
     productServices,
+    userServices,
 };
--- src/service/userServices.js
+++ src/service/userServices.js
@@ -1,30 +1,30 @@
 const { v4: uuidv4 } = require('uuid');
-const { use } = require('../router/userRouter');
 
 const users = [
+
     {
-        "_id": uuidv4(),
+        "_id": "dfc1e13f-bc4d-445b-858f-b16e9c552523",
         "name": "John Doe",
         "email": "johndoe@example.com",
         "age": 30,
         "role": "Admin"
     },
     {
-        "_id": uuidv4(),
+        "_id": "dbb00ffe-83d1-4be5-9f4b-8d76e797c7d7",
         "name": "Jane Smith",
         "email": "janesmith@example.com",
         "age": 28,
         "role": "Editor"
     },
     {
-        "_id": uuidv4(),
+        "_id": "388237b5-332c-4578-8b18-3c7901934c90",
         "name": "Alice Johnson",
         "email": "alicejohnson@example.com",
         "age": 25,
         "role": "Viewer"
     },
     {
-        "_id": uuidv4(),
+        "_id": "a5248219-0a28-429c-aea7-4c6ebdd7afcf",
         "name": "Bob Brown",
         "email": "bobbrown@example.com",
         "age": 35,
@@ -44,7 +44,7 @@ const updatedUser = (id, payload) => {
     const updatedUserIndex = users.findIndex((user) => user._id === id);
 
     if (updatedUserIndex === -1) {
-        throw new Error(` No user with id ${id}`);
+        throw new console.error(` No user with id ${id}`);
     }
 
     users[updatedUserIndex] = { ...users[updatedUserIndex], ...payload };
@@ -54,8 +54,8 @@ const updatedUser = (id, payload) => {
 const deleteUser = (id) => {
     const deleteUserIndex = users.findIndex((user) => user._id === id);
 
-    if(deleteUserIndex === -1) {
-        throw new Error(` No user with id ${id}`);
+    if (deleteUserIndex === -1) {
+        throw new console.error(` No user with id ${id}`);
     }
 
     users.splice(deleteUserIndex, 1);
