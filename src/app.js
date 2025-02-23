da55bbc12d824ce0f274ea81fbd25a28b6c31b36
nazma98 <nazmasarker1311@gmail.com>
2025-02-23 12:43:23 +0600

    Add middleware to check date, request method and url

--- src/app.js
+++ src/app.js
@@ -7,6 +7,11 @@ const app = express()
 
 app.use(express.json())
 
+app.use((req, res, next) => {
+  console.log(`${Date()} - ${req.method} - ${req.url}`);
+  next();
+});
+
 configureRouter(app);
 
 app.listen(port, () => {
