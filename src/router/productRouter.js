b64b3361380cbc22586564c59bcad26584db20d7
nazma98 <nazmasarker1311@gmail.com>
2025-02-23 11:05:32 +0600

    Remove product json from productRouter js

--- src/router/productRouter.js
+++ src/router/productRouter.js
@@ -4,30 +4,6 @@ const productRouter = express.Router();
 
 const { productServices } = require('../service');
 
-const products = [
-    {
-        "_id": "7c08c0d2-5ac7-4cab-afbf-5b379a7f9e73",
-        "name": "beef",
-        "price": 1.4,
-        "image": "beef.jpg",
-        "type": "meat"
-    },
-    {
-        "_id": "eb64c741-6859-4ff8-b3c0-ff6f55b66b13",
-        "name": "baked beans",
-        "price": 0.4,
-        "image": "beans.jpg",
-        "type": "vegetables"
-    },
-    {
-        "_id": "b137eb7c-40ab-497f-836e-a69d4c709111",
-        "name": "hot dogs",
-        "price": 1.99,
-        "image": "hotdogs.jpg",
-        "type": "meat"
-    }
-];
-
 productRouter.get('/', (req, res) => {
     res.send(productServices.getAllProducts());
 });
@@ -41,20 +17,13 @@ productRouter.post('/', (req, res) => {
 productRouter.put('/:id', (req, res) => {
     const { id } = req.params;
     const payload = req.body;
-
     const updatedProduct = productServices.updateProduct(id, payload);
     res.status(201).json(updatedProduct);
 });
 
 productRouter.delete('/:id', (req, res) => {
     const { id } = req.params;
-    const productIndex = products.findIndex((product) => product._id === id);
-
-    if (productIndex === -1) {
-        return res.status(400).json({ message: `No product available with id ${id}` });
-    }
-
-    products.splice(productIndex, 1);
+    productServices.deleteProduct(id);
     res.status(201).json({ message: 'Product has been deleted' });
 });
 
