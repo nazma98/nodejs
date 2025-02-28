d5f89e6039faba48004ad2e5174828ff48498a70
nazma98 <nazmasarker1311@gmail.com>
2025-02-28 08:02:59 +0600

    Add rate limiting

--- src/app.js
+++ src/app.js
@@ -4,14 +4,24 @@ const { logRequestMiddleware, errorHandler } = require('./middleware');
 const { configureRouter } = require('./router');
 const connectDB = require('../db');
 const config = require('./config');
+const { default: rateLimit } = require('express-rate-limit');
+
+const limiter  = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    limit: 1000,
+    standardHeaders: 'draft-8',
+    legacyHeaders: false,
+});
 
 const port = config.PORT;
 
-const app = express()
+const app = express();
+
+app.use(limiter);
 
 connectDB();
 
-app.use(express.json())
+app.use(express.json());
 
 app.use(logRequestMiddleware);
 
